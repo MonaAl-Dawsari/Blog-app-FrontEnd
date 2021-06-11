9dc69311b91e41a40c8c26b2edc37554abdcd07b
Alsaiqal <mona.alsaiqal@live.co.uk>
2021-06-11 16:44:14 +0300

    copy from Mona

--- blog-app-frontend/src/Pages/posts/Newpost.jsx
+++ blog-app-frontend/src/Pages/posts/Newpost.jsx
@@ -10,7 +10,6 @@ export default function Newpost() {
   const [desc, setDesc] = useState("");
   const [file, setFile] = useState(null);
   const { user } = useContext(Context)
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     const newPost = {
@@ -31,42 +30,39 @@ export default function Newpost() {
     try {
       const res = await axios.post("http://localhost:5000/blog/v1/posts/create", newPost);
       window.location.replace("/post/" + res.data._id);
-    } catch (err) { } 
+    } catch (err) { }
   };
-
   return (
-
     <div className="home">
-    
-    <div className="new">
-      
-      {file && (
-      <img className="blogImg" alt="" src={URL.createObjectURL(file)} />
-      )}
-      <div></div>
-      <form action="" className="newForm" onSubmit={handleSubmit}>
-        <div className="newPost">
-          <label htmlFor="fileInput">
-            <i className="fas fa-plus-circle writeIcon "></i>
-          </label>
-          <input type="file" id="fileInput" style={{ display: "none" }} onChange={(e) => setFile(e.target.files[0])}/>
-          <input type="text" placeholder="Title" className="titleStyle" autoFocus={true}  onChange={e=>setTitle(e.target.value)}/>
-        </div>
-        <form className="newForm">
-          <textarea
-            className="titleStyle writeText"
-            placeholder="Tell the World...."
-            type="text"
-            autoFocus={true}
-            onChange={e=>setDesc(e.target.value)}
-          />
-        </form>
-        <button className="btnSubmit" type="submit">
-          Publish
+      <div className="new">
+
+        {file && (
+          <img className="blogImg" alt="" src={URL.createObjectURL(file)} />
+        )}
+        <div></div>
+        <form action="" className="newForm" onSubmit={handleSubmit}>
+          <div className="newPost">
+            <label htmlFor="fileInput">
+              <i className="fas fa-plus-circle writeIcon "></i>
+            </label>
+            <input type="file" id="fileInput" style={{ display: "none" }} onChange={(e) => setFile(e.target.files[0])} />
+            <input type="text" placeholder="Title" className="titleStyle" autoFocus={true} onChange={e => setTitle(e.target.value)} />
+          </div>
+          <form className="newForm">
+            <textarea
+              className="titleStyle writeText"
+              placeholder="Tell the World...."
+              type="text"
+              autoFocus={true}
+              onChange={e => setDesc(e.target.value)}
+            />
+          </form>
+          <button className="btnSubmit" type="submit">
+            Publish
         </button>
-      </form>
-    </div>
-    <Sidebar />
+        </form>
+      </div>
+      <Sidebar />
     </div>
   )
 }
